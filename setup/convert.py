b74a350b11ec695992e4658fd406ed313e87f233
motleytech <motleytechnet@gmail.com>
2015-03-14 20:16:11 -0700

    creating both full and basic html exports

--- setup/convert.py
+++ setup/convert.py
@@ -3,7 +3,12 @@ import os
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.dirname(THIS_DIR)
 
-convert_cmd = "ipython nbconvert --profile=pybook ../books/*.ipynb"
-command = "cd %s; source env.sh; cd export; %s" % (ROOT_DIR, convert_cmd)
+convert_cmd = "ipython nbconvert --profile=pybook --template full ../../books/*.ipynb"
+command = "/bin/bash -c 'cd %s; source env.sh; cd export/full; %s'" % (ROOT_DIR, convert_cmd)
+print "Running command :\n%s\n" % command
+os.system(command)
+
+convert_cmd = "ipython nbconvert --profile=pybook --template basic ../../books/*.ipynb"
+command = "/bin/bash -c 'cd %s; source env.sh; cd export/basic; %s'" % (ROOT_DIR, convert_cmd)
 print "Running command :\n%s\n" % command
 os.system(command)
