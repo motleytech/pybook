a3c67716debbc0921273001a7b2b28a3e948e325
motleytech <motleytechnet@gmail.com>
2015-03-26 14:42:01 -0700

    adding scipy to the install process

--- setup/install.py
+++ setup/install.py
@@ -28,6 +28,14 @@ os.system("/bin/bash -c 'cd %s; source %s; python -m IPython.external.mathjax'" 
 # install pandoc to export to pdf
 os.system("sudo apt-get -y install pandoc")
 
+# dependencies for scipy
+os.system("sudo apt-get -y install libamd2.2.0 libblas3gf libc6 libgcc1\
+ libgfortran3 liblapack3gf libumfpack5.4.0 libstdc++6\
+ build-essential gfortran libatlas-base-dev python-all-dev")
+
+os.system("/bin/bash -c 'cd %s; source %s; pip install scipy'" %
+          (ROOT_DIR, "env.sh"))
+
 
 # upgrade distribute
 os.system("/bin/bash -c 'cd %s; source %s; pip install distribute --upgrade'" %
