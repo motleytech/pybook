5388d008ad88948d2d391adcb16e392b0ffdad86
motleytech <motleytechnet@gmail.com>
2015-09-09 21:42:23 -0700

    adding requirements.txt and updating install to use it

--- setup/install.py
+++ setup/install.py
@@ -17,17 +17,6 @@ else:
         os.rmdir(VENV_FOLDER)
         os.system("cd %s; virtualenv %s" % (ROOT_DIR, VENV_NAME))
 
-# upgrade pip
-os.system("/bin/bash -c 'cd %s; source %s; pip install -U pip'" % (ROOT_DIR, "env.sh"))
-
-# install pip dependencies
-os.system("/bin/bash -c 'cd %s; source %s; pip install ipython[notebook]'" %
-          (ROOT_DIR, "env.sh"))
-
-# install mathjax
-os.system("/bin/bash -c 'cd %s; source %s; python -m IPython.external.mathjax'" %
-          (ROOT_DIR, "env.sh"))
-
 # install pandoc to export to pdf
 os.system("sudo apt-get -y install pandoc")
 
@@ -36,16 +25,16 @@ os.system("sudo apt-get -y install libamd2.2.0 libblas3gf libc6 libgcc1\
  libgfortran3 liblapack3gf libumfpack5.4.0 libstdc++6\
  build-essential gfortran libatlas-base-dev python-all-dev")
 
-os.system("/bin/bash -c 'cd %s; source %s; pip install scipy'" %
-          (ROOT_DIR, "env.sh"))
+# dependencies for matlab
+os.system("sudo apt-get -y install libfreetype6-dev libpng12-dev")
 
+
+# upgrade pip
+os.system("/bin/bash -c 'cd %s; source %s; pip install -U pip'" % (ROOT_DIR, "env.sh"))
 
 # upgrade distribute
 os.system("/bin/bash -c 'cd %s; source %s; pip install distribute --upgrade'" %
           (ROOT_DIR, "env.sh"))
 
-# install matplotlib
-os.system("sudo apt-get -y install libfreetype6-dev libpng12-dev")
-os.system("/bin/bash -c 'cd %s; source %s; pip install matplotlib'" %
-          (ROOT_DIR, "env.sh"))
-
+os.system("/bin/bash -c 'cd %s; source %s; pip install -r %s/setup/requirements.txt'" %
+          (ROOT_DIR, "env.sh", ROOT_DIR))
