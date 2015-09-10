9913f7c141583c41c5f7f5e6384cb4ba2f16ed5d
motleytech <motleytechnet@gmail.com>
2015-09-09 22:16:32 -0700

    update convert script

--- setup/convert.py
+++ setup/convert.py
@@ -3,12 +3,12 @@ import os
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.dirname(THIS_DIR)
 
-convert_cmd = "ipython nbconvert --profile=pybook --template full ../../books/*.ipynb"
+convert_cmd = "jupyter nbconvert --template full ../../books/*.ipynb"
 command = "/bin/bash -c 'cd %s; source env.sh; cd export/full; %s'" % (ROOT_DIR, convert_cmd)
 print "Running command :\n%s\n" % command
 os.system(command)
 
-convert_cmd = "ipython nbconvert --profile=pybook --template basic ../../books/*.ipynb"
+convert_cmd = "jupyter nbconvert --template basic ../../books/*.ipynb"
 command = "/bin/bash -c 'cd %s; source env.sh; cd export/basic; %s'" % (ROOT_DIR, convert_cmd)
 print "Running command :\n%s\n" % command
 os.system(command)
