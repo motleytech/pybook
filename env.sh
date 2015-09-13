0821bc035fdfb47039074c41678605dde094eb15
motleytech <motleytechnet@gmail.com>
2015-09-12 23:57:42 -0400

    adding custom.js to repo. changing jupyter data dir

--- env.sh
+++ env.sh
@@ -3,4 +3,5 @@ export JUPYTER_CONFIG_DIR=`pwd`/config
 export IPYTHONDIR=`pwd`/.ipython
 export PATH=/usr/local/texlive/2015/bin/x86_64-linux:$PATH
 export JUPYTER_DATA_DIR=`pwd`/jupyter_data
+export JUPYTER_PATH=`pwd`/extensions
 
