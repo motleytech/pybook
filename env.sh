ed61339a7b9b203624d193b329a6a29b063a9731
motleytech <motleytechnet@gmail.com>
2015-09-12 23:46:38 -0400

    adding jupyter data dir and loading custom.js extension

--- env.sh
+++ env.sh
@@ -2,3 +2,5 @@ source venv/bin/activate
 export JUPYTER_CONFIG_DIR=`pwd`/config
 export IPYTHONDIR=`pwd`/.ipython
 export PATH=/usr/local/texlive/2015/bin/x86_64-linux:$PATH
+export JUPYTER_DATA_DIR=`pwd`/jupyter_data
+
