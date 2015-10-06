25ab97bc5cea2c47f47e10f89ab33d4c47cbd66d
motleytech <motleytechnet@gmail.com>
2015-10-06 14:32:18 -0400

    adding timeit to startup script

--- .ipython/profile_default/startup/20-custom.py
+++ .ipython/profile_default/startup/20-custom.py
@@ -1,6 +1,7 @@
 import io
 from IPython.nbformat import current
 from pprint import pprint as pp
+from time import time
 
 def execute_notebook(nbfile):
     
@@ -15,7 +16,24 @@ def execute_notebook(nbfile):
         ip.run_cell(cell.input)
     return
 
+def timeit(func):
+    def wrapper(*a, **kw):
+        startTime = time()
+        try:
+            rv = func(*a, **kw)
+        except:
+            raise
+        finally:
+            endTime = time()
+            print "Time = {etime}".format(etime=(endTime - startTime))
+        return rv
+    return wrapper
+
+
+
 gl = globals()
 
 gl['execute_notebook'] = execute_notebook
 gl['pp'] = pp
+gl['timeit'] = timeit
+
