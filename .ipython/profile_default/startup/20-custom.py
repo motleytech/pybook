aaf2a668dc06412f72480ee51684b649622ec6ba
Nagarajan <nagarajan@cyaninc.com>
2015-10-06 11:30:38 -0700

    changing the __name__ for imported notebooks

--- .ipython/profile_default/startup/20-custom.py
+++ .ipython/profile_default/startup/20-custom.py
@@ -2,17 +2,27 @@ import io
 from IPython.nbformat import current
 from pprint import pprint as pp
 
-def execute_notebook(nbfile):
-    
+def execute_notebook(nbfile, name=None):
+    global __name__
     with io.open(nbfile) as f:
         nb = current.read(f, 'json')
-    
+
     ip = get_ipython()
-    
-    for cell in nb.worksheets[0].cells:
-        if cell.cell_type != 'code':
-            continue
-        ip.run_cell(cell.input)
+
+    if name is None:
+        name = ".".join(nbfile.split(".")[:-1])
+
+    old_name = __name__
+    __name__ = name
+
+    try:
+        for cell in nb.worksheets[0].cells:
+            if cell.cell_type != 'code':
+                continue
+            ip.run_cell(cell.input)
+    finally:
+        __name__ = old_name
+
     return
 
 gl = globals()
